72bd04d6381d68ecc13da2e1b2d6d53dfbc4f160
maxduh <37228674+maxduh@users.noreply.github.com>
2021-10-23 17:35:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+function drag_start(event) {
+    var style = window.getComputedStyle(event.target, null);
+    var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY) + ',' + event.target.id;
+    event.dataTransfer.setData("Text", str);
+
+}
+
+function drop(event) {
+    var offset = event.dataTransfer.getData("Text").split(',');
+    var dm = document.getElementById(offset[2]);
+    dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+    event.preventDefault();
+    return false;
+}
+
+function drag_over(event) {
+    event.preventDefault();
+    return false;
+}
+var list = document.getElementById('list');
+var num = 3;
+
+function changeText2(e) {
+    var path = e.slot;
+    var node = document.createElement("LI");                
+    var myImage = new Image(80,80);
+    myImage.src = path;
+    myImage.draggable=false;      
+    node.appendChild(myImage);   
+    node.className="item";
+    node.draggable=true;       
+    node.ondragstart=function(){drag_start(event)};
+    num +=1;
+    node.id="txt"+num;
+    document.getElementById("list").appendChild(node);
+}
+
+
+
+
+var previewimg=document.getElementById("previewimg");
+
+
+
+//********************menu***************************\\
+
+
+function menu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.menu-button');
+    let items = menu.find('.menu-item');
+    let preview = menu.find('.preview');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    items.on('mouseenter', (event) => {
+        let target = event.target;
+        if (target.className == 'menu-item') {
+        previewimg.src=target.slot;
+            
+            
+        }
+    });
+
+    items.on('mouseleave', (event) => {
+        button.css('background-color','white');
+    });
+
+    function toggleMenu() {
+        menu.toggleClass('menu-active');
+        if (menu.hasClass('menu-active')) {
+            preview.css('left', '230px');
+            $('body').css('overflow', 'hidden')
+        } else {
+            $('body').css('overflow', 'visible')
+            preview.css('left', '-10%');
+        }
+    }
+}
+
+import json from 'assets/Chars/index.json'
+
+
+console.log(json);
+
+menu('.menu');
---
+++ music.js
@@ -0,0 +1,100 @@
+var i=0;
+var a1=0;
+var a2=0;
+var v1=0;
+var v2=0;
+var e1 = document.getElementById("audio1");
+var e2 = document.getElementById("audio2");
+var ep1 = document.getElementById("plate1");
+var ep2 = document.getElementById("plate2");
+const clamp = (num) => Math.min(Math.max(num, 0), 1);
+var volumeE = document.getElementById("volume");
+let volume=100;
+var d1 =  document.getElementById("d1");
+var d2 =  document.getElementById("d2");
+
+var s1 =  document.getElementById("s1");
+var s2 =  document.getElementById("s2");
+
+var pb1 =  document.getElementById("pb1");
+var pb2 =  document.getElementById("pb2");
+
+var rt1=0;
+var rt2=0;
+
+
+
+
+
+volumeE.oninput = function() {
+    volume = this.value;
+  }
+
+function Set(q){
+    if(q==1){
+        s1.src=d1.value;
+        e1.load();
+        e1.play();
+        e1.currentTime = 0;
+        a1=2;
+        a2=1;
+    }else if(q==2){
+        s2.src=d2.value;
+        e2.load();
+        e2.play();
+        e2.currentTime = 0;
+        a1=1;
+        a2=2;
+        
+    }
+    
+}
+
+function Update(){
+    if(a1==2){
+        if(v1<1){
+        v1+=0.005}else{
+            v1=1
+            a1=3
+        }
+    }else if(a1==1){
+        if(v1>-0.75){
+            v1-=0.005
+        }else{
+                v1=-0.75
+                a1=0
+            }
+    }
+    if(a2==2){
+        if(v2<1){
+        v2+=0.005}else{
+            v2=1
+            a2=3
+        }
+    }else if(a2==1){
+        if(v2>-0.75){
+            v2-=0.005
+        }else{
+                v2=-0.75
+                a2=0
+            }
+    }
+
+    if(v1>0){
+        rt1+=v1*2;
+    }
+    ep1.style.transform = `rotate(${rt1}deg)`;
+    if(v2>0){
+        rt2+=v2*2;
+    }
+    if(rt1>360){rt1-=360}
+    if(rt2>360){rt2-=360}
+    ep2.style.transform = `rotate(${rt2}deg)`;
+    e1.volume=clamp(v1)*(volume/100);
+    e2.volume=clamp(v2)*(volume/100);
+    pb1.style.width=  `${(e1.currentTime/e1.duration*100)}%`;
+    pb2.style.width=  `${(e2.currentTime/e2.duration*100)}%`;
+    setTimeout(Update, 33);
+}
+
+Update();
